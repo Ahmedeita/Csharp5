030620b756751b9adb6f4f2456e0efff4ded98a2
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-25 21:33:36 +0200

    Casting [Boxing - Unboxing]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            #region Part 01 Boxing, UnBoxing
+
+            #region Casting [Boxing - Unboxing]
+
+            // Reference Type Casting => Convert from Reference to To Reference Type
+            // Boxing Casting => Convert from Value Type to Reference Type
+            // UnBoxing Casting => Convert from Reference Type to Value Type
+
+            //object obj = new object(); 
+            //obj = "Mostafa"; // Reference Type Casting
+
+            //obj = 2; // Boxing [Casting from Value Type [int] to Reference Type [Object]
+
+            #region Boxing [Safe Code]
+            //int X = 10; 
+            //object obj01 = new object(); 
+            //obj01 = X; 
+            //Console.WriteLine(obj01); 
+            // Parent = Child
+
+            #endregion
+            #region Unboxing [Un Safe Code]
+            //object obj02 = new object();
+            //obj02 = 10;
+            //int X = (int)obj02;
+            //Child = Parent
+            #endregion
 
             #endregion
         }
